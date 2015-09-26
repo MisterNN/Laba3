d921bc30612652018ca8f448bfdc9f26b02d9a67
MisterNN <mr.cnas@mail.ru>
2015-09-26 18:16:55 +0300

    fads

---
+++ number2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int maxmultiply(int mass[], int number)
+{
+	sort(mass,mass + number);
+	for(int i = 0;i <= number - 1;i++)
+	{
+		cout << mass[i] << endl;
+	}
+	int z = mass[0] * mass[1] * mass[number - 1];
+	int x = mass[number - 1] * mass[number - 2] * mass[number - 3];
+	int y = max(z,x);
+	cout << y << endl;
+	return y;
+}
+
+int main()
+{
+	int mass[5] = {-1,-2,0,-3328,2};
+	maxmultiply(mass,5);
+}
