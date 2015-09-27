8a2fd2e3d4c7abbc49dbf876637b44a0ca5ec7d6
MisterNN <mr.cnas@mail.ru>
2015-09-27 19:10:20 +0300

    Update number2.cpp
    my own test deleted

--- number2.cpp
+++ number2.cpp
@@ -5,17 +5,11 @@ using namespace std;
 int maxmultiply(int mass[], int number)
 {
 	sort(mass,mass + number);
-	for(int i = 0;i <= number - 1;i++)
-	{
-		cout << mass[i] << endl;
-	}
 	int z = mass[0] * mass[1] * mass[number - 1];
 	int x = mass[number - 1] * mass[number - 2] * mass[number - 3];
 	int y = max(z,x);
-	cout << y << endl;
 	return y;
 }
-
 int main()
 {
 }
